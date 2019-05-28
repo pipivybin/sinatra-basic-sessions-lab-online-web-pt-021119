69b857809e444f8f7518b5e29823b2aab2b1ec16
Ming Duan <11203duan@gmail.com>
2019-05-28 21:46:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+configure do
+  enable :sessions
+  set :session_secret, "jonsnow"
+end
+
+get '/' do
+  @session = session
+  erb :index
+end
+
+post '/checkout' do
+  @session["item"] = params[:item]
+  erb :checkout
+end
+
 end
